f43eb54fd4d8072cf2744bf133d01fe292a5d64b
AL Zobaer <al.zobaer@yahoo.com>
2024-01-23 10:49:58 +0600

    all changes to be commited

--- MPI/task3_mpi_phonebook_search.c
+++ MPI/task3_mpi_phonebook_search.c
@@ -3,52 +3,34 @@
 #include <string.h>
 #include <mpi.h>
 
+#define MAX_FILES 10
+#define MAX_LINE_LENGTH 1000
 #define MAX_NAME_LENGTH 50
 #define MAX_PHONE_LENGTH 15
 
-typedef struct {
-    char name[MAX_NAME_LENGTH];
-    char phone[MAX_PHONE_LENGTH];
-} Contact;
-
-// Function to search for a name in a range of contacts and print matching results
-void searchPhonebook(char names[][50], char numbers[][50], char* searchName, int start, int end) {
-    for (int i = start; i < end; i++) {
-        // Case-sensitive search for the given name in the phonebook
-        if (strstr(names[i], searchName) != NULL){
-            // Print the matching name and corresponding phone number
-            printf("%s\t%s\n", names[i], numbers[i]);
-        }
-    }
-}
-
 int main(int argc, char **argv) {
-    char names[1000][50];
-    char contacts[1000][50];
-    char searchName[50];
+    char names[MAX_LINE_LENGTH][MAX_NAME_LENGTH];
+    char contacts[MAX_LINE_LENGTH][MAX_PHONE_LENGTH];
+    char searchName[MAX_NAME_LENGTH];
 
     int id, size;
     int line_count = 0;
 
     double cal_start_time, cal_end_time;
 
-    // Initialize MPI
     MPI_Init(&argc, &argv);
 
     // Get the rank (id) and size of the MPI communicator
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    const int MAX_FILES = 10;
-    const char* phonebookFiles[MAX_FILES];
+    char *phonebookFiles[MAX_FILES];
 
     // the 'argc' (the number of command-line arguments) will be 1, as the program name itself is counted as an argument
     if (argc < 3) {
         if (id == 0) {
-            // Print an error message if usage is incorrect
             fprintf(stderr, "Usage: %s <phonebook_file1> [<phonebook_file2> ...]\n", argv[0]);
         }
-        // Finalize MPI and return an error code
         MPI_Finalize();
         return 1;
     }
@@ -58,25 +40,24 @@ int main(int argc, char **argv) {
         phonebookFiles[i - 1] = argv[i];
     }
 
+    cal_start_time = MPI_Wtime();
+
     // Read and process phonebook files on the root process (id == 0)
     if (id == 0) {
         char c;
         FILE* file;
 
-        // Loop through each specified phonebook file
         for (int i = 0; i < argc - 1; i++) {
             // Open the current phonebook file for reading
             file = fopen(phonebookFiles[i], "r");
             
-            // Check if the file opening was successful
             if (file == NULL) {
-                // Print an error message and abort MPI if opening fails
                 fprintf(stderr, "Error opening file: %s\n", phonebookFiles[i]);
                 MPI_Abort(MPI_COMM_WORLD, 1);
             }
 
             // Read lines from the file and store names and contacts in arrays
-            while (fscanf(file, "%49[^,],%111[^\n]\n", &names[line_count], &contacts[line_count]) != EOF) {
+            while (fscanf(file, "%49[^,],%14[^\n]\n", &names[line_count], &contacts[line_count]) != EOF) {
                 line_count += 1;
             }
 
@@ -85,10 +66,8 @@ int main(int argc, char **argv) {
         // Prompt the user to enter the name to search for
         scanf("%s", searchName);
     }
-
-    cal_start_time = MPI_Wtime();
     
-    // Broadcast the line count, search name, and phonebook data to all processes
+    // Broadcast the line count, search name to all processes
     MPI_Bcast(&line_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(searchName, 50, MPI_CHAR, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
@@ -98,6 +77,9 @@ int main(int argc, char **argv) {
         MPI_Bcast(names[i], 50, MPI_CHAR, 0, MPI_COMM_WORLD);
         MPI_Bcast(contacts[i], 50, MPI_CHAR, 0, MPI_COMM_WORLD);
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
     // Divide the work among processes based on line count and process ID
     int width = line_count / size;
     int start = id * width;
@@ -105,18 +87,23 @@ int main(int argc, char **argv) {
 
     // Determine the end index for the current process
     if (id == (size - 1))
-        end = line_count;
+        end = line_count;   // higher rank executes till last line
     else
         end = (id + 1) * width;
+        
+    // search for a name in a range of contacts and print matching results
+    for (int i = start; i < end; i++) {
+        // Case-sensitive search for the given name in the phonebook
+        if (strstr(names[i], searchName) != NULL){      // searches for the substring searchName within the string names[i]
+            //printf("%s\t%s\n", names[i], contacts[i]);
+        }
+    }
 
-    // Call the search function for the assigned range of contacts
-    searchPhonebook(names, contacts, searchName, start, end);
-
-    MPI_Barrier(MPI_COMM_WORLD);
 
     cal_end_time = MPI_Wtime();
 
-    printf("PID %d: Total time: %lf\n", id, cal_end_time - cal_start_time);   
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("PID %d: Total time: %lf\n", id, (cal_end_time - cal_start_time));   
 
     MPI_Finalize();
 
